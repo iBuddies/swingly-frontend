cb0dbaa53b35f213fc7360913b7dc021a6394c9d
Srihith <srihith.maraka@gmail.com>
2025-11-18 23:32:23 -0800

    chore: remove old Vite webapp

--- webapp/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Using React plugin is optional when not transforming, but keep for DX if installed later.
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 5173, host: true }
-})
-
